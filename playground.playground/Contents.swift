c4e4e79b1ef4b47667836761a0574e9a824f1357
Christian Tisby <christian.d.tisby@gmail.com>
2022-09-10 16:09:32 -0400

    variables playground

---
+++ playground.playground/Contents.swift
@@ -0,0 +1,30 @@
+import UIKit
+
+/// declaring variables
+// var myNumber: Int
+
+///Initialization
+//var myNumber: Int
+//myNumber = 5
+
+
+/// declare and Initialize variable in the same statement
+//var myNumber: Int = 5
+//myNumber = 87
+
+/// declaring variables of different types
+//var myNumber: Int = 5
+//var myFavorite: Float = 14.129
+
+
+/// declaring variables without specifying the type
+//var myNumber = 5
+//var myFavorite = 14.129
+
+
+/// ARITHMATIC OPERATORS
+/// assigning the result of an operation to a variable
+
+var myNumber = 5 + 10
+
+
