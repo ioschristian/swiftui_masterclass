b5bf5f41ba37988475dcda40592f03df8d3df598
Christian Tisby <christian.d.tisby@gmail.com>
2022-09-09 22:22:24 -0400

    first commit

---
+++ AsyncAwait/AsyncAwait/AsyncAwaitApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncAwaitApp.swift
+//  AsyncAwait
+//
+//  Created by Christian Tisby on 9/9/22.
+//
+
+import SwiftUI
+
+@main
+struct AsyncAwaitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
